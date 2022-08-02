7b71335e0d99742e87f0c8353f364a7b362ceb90
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-02 10:20:30 -0500

    test

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - creates binary tree node
  * @parent: points to the parent node of node to create
  * @value: value to put into new node
- * @Retrun: returns pointer to a new node, null otherwise
+ * Return: returns pointer to a new node, null otherwise
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
