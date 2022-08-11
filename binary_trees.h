135b4928558a07b83ba649b2cee564beb263559c
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-11 11:18:23 -0500

    updating prototype file

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,11 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
-#include <stdio.h>
+#ifndef _BINARY_TREE_
+#define _BINARY_TREE_
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,11 +21,16 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-typedef struct binary_tree_s binary_tree_t;
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree);
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
@@ -39,4 +48,13 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+
 #endif
